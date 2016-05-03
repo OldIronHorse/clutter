90f7bc5acf9561b93e09a2a7a02d69de48fbdc25
Simon Rdding <s1m0n.r3dd1ng@gmail.com>
2016-05-03 15:27:51 +0100

    Basic project configured with POC code.

--- project.clj
+++ project.clj
@@ -1,9 +1,16 @@
 (defproject clutter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot clutter.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :description "Chatroom webservice"
+  :url "https://github.com/OldIronHorse/clutter.git"
+  :license {:name "GNU Public License v3"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [compojure "1.5.0"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler clutter.handler/app
+         :nrepl {:start? true
+                 :port 9998}}
+  profiles 
+    {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                          [ring-mock "0.1.5"]]}})
